d63010d07c71da3bab3a67850ead58fb4aaf3fa7
Jacob Yaw <yawjacob@yahoo.com>
2022-09-14 20:47:36 -0500

    built out fetch, like/unlike, and search feature

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,12 +1,27 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ search }) {
+
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then(r => r.json())
+      .then(listings => setListings(listings))
+  }, [])
+
+  const filteredListings = listings.filter(listing => {
+    return listing.description.toLowerCase().includes(search.toLowerCase())
+  })
+
+  const listingCards = filteredListings.map(listingObj => {
+    return <ListingCard key={listingObj.id} listing={listingObj} />
+  })
+
   return (
     <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
-      </ul>
+      <ul className="cards">{listingCards}</ul>
     </main>
   );
 }
